d2657738ccc3c8d023c43e2f95a240ae4d41a775
Sam Faull <samz5991@hotmail.co.uk>
2017-09-02 11:35:35 +0100

    Added code header and basic IO code to make LED refelct button state

--- generic_debounce.ino
+++ generic_debounce.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+/* 
+ * Author: Sam Faull
+ * Details: Generic doebounce code to avoid bouncing when using a pushswitch
+ * 
+ * Pin allocations: 
+ * Pushswitch = D2, LED =  D13
+ */
 
+#define LED 13
+#define Button 2
+
+bool ButtonState;
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  ButtonState = digitalRead(Button);
+  if (ButtonState)
+    digitalWrite(13, HIGH);
+  else
+    digitalWrite(13, LOW);
+  delay(10);
 
 }
